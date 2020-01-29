bbd6bbfd6a65d2d1dc909a8746d8e4934d44f485
seongjinbak <adnysstudio@gmail.com>
2020-01-29 13:47:08 +0900

    로비 씬 구현
    - 네트워크 연결 안함.
    - 채팅창 구현. InputField에 글자 입력 후 엔터 누르면 입력된 문자가 채팅창에 뜬다.
    - receive 했을 때 receive한 내용을 ChattingManager.cs의 dialogue에 붙혀주면 될듯.
    
    - 매치메이킹 버튼, 수락/거절창 구현
    - MatchingManager의 isMatchmakingCompleted를 체크하면 수락/거절창이 뜸.
    - 거절창 클릭시 초기 로비화면으로 돌아옴.

---
+++ src/unity/Assets/Scripts/Chatting/ChattingManager.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * 로비에서의 채팅창을 관리하는 매니저
+ */ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChattingManager : MonoBehaviour
+{
+    // 입력창
+    public InputField inputField;
+
+    // 역대 채팅 내역을 기록할 문자열
+    public Text dialogue;
+
+    // 스크롤 바
+    public ScrollRect scrollbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 아무 입력이 없다면
+        if(inputField.text.Length == 0)
+        {
+            return;
+        }
+
+        // 입력 텍스트의 마지막 문자가 엔터라면
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            dialogue.text += inputField.text;
+            dialogue.text += "\n";
+            inputField.text = "";
+            scrollbar.verticalNormalizedPosition = 0f;
+        }
+        
+    }
+
+    
+
+}
