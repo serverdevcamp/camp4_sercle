2cf81564442b5ec8292f3d2e179f45b32ac2875d
EQwon <bluetwintail0906@gmail.com>
2020-01-29 12:28:49 +0900

    빌드 환경에서 에디터 코드를 무시하도록 수정

--- src/unity/Assets/Scripts/Custom Editor/SkillEffectEditor.cs
+++ src/unity/Assets/Scripts/Custom Editor/SkillEffectEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(SkillEffect)), CanEditMultipleObjects]
@@ -68,3 +69,4 @@ public class SkillEffectEditor : Editor
         serializedObject.ApplyModifiedProperties();
     }
 }
+#endif
