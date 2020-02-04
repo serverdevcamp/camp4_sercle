afe578760af5dfdc331b44af992783904ef70666
EQwon <bluetwintail0906@gmail.com>
2020-02-04 11:27:59 +0900

    캐릭터 선택 기본적 UI 연결

---
+++ src/unity/Assets/Scripts/CustomSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomSlider : MonoBehaviour
+{
+    [SerializeField] private int max;
+    [SerializeField] private int min;
+    [SerializeField] private int value;
+
+    private float minWidth = 100f;
+    private float maxWidth = 320f;
+
+    [Header("Element Holder")]
+    [SerializeField] private RectTransform sliderBar;
+    [SerializeField] private Text valueText;
+
+    private void UpdateSlider()
+    {
+        float mappedValue = (float)(value - min) / (max - min);
+        float targetWidth = minWidth + (maxWidth - minWidth) * mappedValue;
+        float originHeight = sliderBar.sizeDelta.y;
+
+        sliderBar.sizeDelta = new Vector2(targetWidth, originHeight);
+        valueText.text = value.ToString();
+    }
+
+    public void SetValue(int value)
+    {
+        this.value = value;
+        UpdateSlider();
+    }
+}
---
+++ src/unity/Assets/Scripts/UIManager_Matching.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_Matching : MonoBehaviour
+{
+    [Header("Character Prefab")]
+    public List<Character> characters;
+
+    [Header("Character Basic Info")]
+    [SerializeField] private GameObject characterInfoPanel;
+    [SerializeField] private Text characterName;
+    [SerializeField] private List<CustomSlider> statusSliders;
+    [SerializeField] private List<Image> skillIcons;
+
+    [Header("Character Skill Info")]
+    [SerializeField] private GameObject skillInfoPanel;
+    [SerializeField] private Text skillName;
+    [SerializeField] private Text skillDescription;
+
+    private void Start()
+    {
+        characterInfoPanel.SetActive(false);
+        skillInfoPanel.SetActive(false);
+    }
+
+
+    public void ShowCharacterInfo(int num)
+    {
+        characterInfoPanel.SetActive(true);
+        skillInfoPanel.SetActive(false);
+
+        Character character = characters[num];
+
+        characterName.text = character.name;
+
+        for(int i = 0; i < statusSliders.Count; i++)
+        {
+            int targetValue;
+            if (i == 0) targetValue = character.status.MHP;
+            else targetValue = (int)character.status.Value((StatusType)(i + 2));
+
+            statusSliders[i].SetValue(targetValue);
+        }
+
+        for (int i = 0; i < character.skills.Count; i++)
+        {
+            Skill skill = character.skills[i];
+            skillIcons[i].GetComponent<Button>().onClick.AddListener(delegate { ShowSkillInfo(skill); });
+        }
+    }
+
+    public void ShowSkillInfo(Skill skill)
+    {
+        skillInfoPanel.SetActive(true);
+
+        skillName.text = skill.skillName;
+        skillDescription.text = skill.description;
+    }
+}
