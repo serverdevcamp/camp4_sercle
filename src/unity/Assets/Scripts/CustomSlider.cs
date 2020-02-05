492bca181d8e0962c75d1708972b82581ba24403
EQwon <bluetwintail0906@gmail.com>
2020-02-05 15:50:29 +0900

    에셋 추가

--- src/unity/Assets/Scripts/CustomSlider.cs
+++ src/unity/Assets/Scripts/CustomSlider.cs
@@ -2,33 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CustomSlider : MonoBehaviour
 {
     [SerializeField] private int max;
     [SerializeField] private int min;
-    [SerializeField] private int value;
+    [SerializeField] private int targetValue;
+    private float curValue;
 
     private float minWidth = 100f;
     private float maxWidth = 320f;
+    private float changingTime = 1f;
 
     [Header("Element Holder")]
     [SerializeField] private RectTransform sliderBar;
     [SerializeField] private Text valueText;
 
+    private void Start()
+    {
+        curValue = min;
+    }
+
+    private void Update()
+    {
+        UpdateSlider();
+    }
+
     private void UpdateSlider()
     {
-        float mappedValue = (float)(value - min) / (max - min);
+        float mappedValue = (curValue - min) / (max - min);
         float targetWidth = minWidth + (maxWidth - minWidth) * mappedValue;
         float originHeight = sliderBar.sizeDelta.y;
 
         sliderBar.sizeDelta = new Vector2(targetWidth, originHeight);
-        valueText.text = value.ToString();
+        valueText.text = curValue.ToString("0");
     }
 
     public void SetValue(int value)
     {
-        this.value = value;
-        UpdateSlider();
+        targetValue = value;
+        DOTween.To(() => curValue, x => curValue = x, targetValue, changingTime);
     }
 }
