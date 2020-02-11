0563e436b577c2c5cb506e5f45ff0361f1019e70
EQwon <bluetwintail0906@gmail.com>
2020-02-11 17:07:53 +0900

    로비 UI 수정

---
+++ src/unity/Assets/Scripts/MatchingProgressBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchingProgressBar : MonoBehaviour
+{
+    [SerializeField] private Image progressBar;
+
+    private void OnEnable()
+    {
+        progressBar.fillAmount = 1f;
+    }
+
+    private void Update()
+    {
+        progressBar.fillAmount -= Time.deltaTime / 10f;
+    }
+}
---
+++ src/unity/Assets/Scripts/UIManager_Lobby.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_Lobby : MonoBehaviour
+{
+    [SerializeField] private GameObject matchBtn;
+    [SerializeField] private GameObject cancelBtn;
+    [SerializeField] private GameObject matchTimer;
+    [SerializeField] private GameObject matchCompletePanel;
+    [SerializeField] private GameObject matchWaitingPanel;
+
+    private MatchingManager matchingManager;
+
+    private void Start()
+    {
+        matchingManager = GameObject.Find("MatchingManager").GetComponent<MatchingManager>();
+
+        matchBtn.SetActive(true);
+        cancelBtn.SetActive(false);
+        matchTimer.SetActive(false);
+        matchCompletePanel.SetActive(false);
+        matchWaitingPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        ShowMatchingUI();
+    }
+
+    // 매치메이킹 요청 등의 결과와 현재 State에 따라 화면에 출력되는 UI를 결정하는 함수.
+    private void ShowMatchingUI()
+    {
+        MatchingManager.MatchingState state = matchingManager.MatchState;
+
+        switch (state)
+        {
+            case MatchingManager.MatchingState.Nothing:
+                matchBtn.SetActive(true);
+                cancelBtn.SetActive(false);
+                matchTimer.SetActive(false);
+                matchCompletePanel.SetActive(false);
+                matchWaitingPanel.SetActive(false);
+                break;
+
+            case MatchingManager.MatchingState.WaitMatchingResult:
+                matchBtn.SetActive(false);
+                cancelBtn.SetActive(true);
+                matchTimer.SetActive(true);
+                matchCompletePanel.SetActive(false);
+                matchWaitingPanel.SetActive(false);
+                break;
+
+            case MatchingManager.MatchingState.SelectMatchingResult:
+                matchBtn.SetActive(false);
+                cancelBtn.SetActive(false);
+                matchTimer.SetActive(false);
+                matchCompletePanel.SetActive(true);
+                matchWaitingPanel.SetActive(false);
+                break;
+
+            case MatchingManager.MatchingState.AcceptMatchingResult:
+                matchBtn.SetActive(false);
+                cancelBtn.SetActive(false);
+                matchTimer.SetActive(false);
+                matchCompletePanel.SetActive(false);
+                matchWaitingPanel.SetActive(true);
+                matchWaitingPanel.GetComponentInChildren<Text>().text = "Waiting For Opponent...";
+                break;
+
+            case MatchingManager.MatchingState.RefuseMatchingResult:
+                matchBtn.SetActive(false);
+                cancelBtn.SetActive(false);
+                matchTimer.SetActive(false);
+                matchCompletePanel.SetActive(false);
+                matchWaitingPanel.SetActive(true);
+                matchWaitingPanel.GetComponentInChildren<Text>().text = "Cancel...";
+                break;
+        }
+    }
+}
