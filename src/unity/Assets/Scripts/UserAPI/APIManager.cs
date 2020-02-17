d8596096e42a87f6f75354dcb60cb1dd6ba8fd3a
EQwon <bluetwintail0906@gmail.com>
2020-02-17 15:56:17 +0900

    newtonsoft 삭제

--- src/unity/Assets/Scripts/UserAPI/APIManager.cs
+++ src/unity/Assets/Scripts/UserAPI/APIManager.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 public class APIManager
 {
-    JArray apiData;
+    //JArray apiData;
 
     public APIManager()
     {
@@ -12,15 +12,15 @@ public class APIManager
     public List<UserAchieveData> GetAchieveData(string data)
     {
         List<UserAchieveData> list = new List<UserAchieveData>();
-        apiData = JArray.Parse(data);
-        foreach (JObject jsonData in apiData)
-        {
-            UserAchieveData tmp = new UserAchieveData();
-            tmp.id = Int32.Parse(jsonData["id"].ToString());
-            tmp.listid = Int32.Parse(jsonData["id"].ToString());
-            tmp.achieve = jsonData["listid"].ToString();
-            list.Add(tmp);
-        }
+        //apiData = JArray.Parse(data);
+        //foreach (JObject jsonData in apiData)
+        //{
+        //    UserAchieveData tmp = new UserAchieveData();
+        //    tmp.id = Int32.Parse(jsonData["id"].ToString());
+        //    tmp.listid = Int32.Parse(jsonData["id"].ToString());
+        //    tmp.achieve = jsonData["listid"].ToString();
+        //    list.Add(tmp);
+        //}
         return list;
     }
 
