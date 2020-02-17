524a8ea8e13c84dd3cbb17b8cd4a57682a84bf1b
EQwon <bluetwintail0906@gmail.com>
2020-02-17 12:05:55 +0900

    Skill Select 기본 기능 구현

---
+++ src/unity/Assets/Scripts/SkillSelection/SkillButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+    [SerializeField] private Image myImage;
+
+    private UIManager_SkillSelect UIManager;
+
+    private int skillNum;
+
+    public void Initialize(UIManager_SkillSelect manager, int num, Sprite image)
+    {
+        UIManager = manager;
+        skillNum = num;
+        myImage.sprite = image;
+
+        gameObject.AddComponent<Button>().onClick.AddListener(() => { SendSkillInfo(); });
+    }
+
+    private void SendSkillInfo()
+    {
+        UIManager.ShowSkillInfo(skillNum);
+    }
+}
