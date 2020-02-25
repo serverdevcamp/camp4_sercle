15e6e999115f7a3a2c47329b27c85e24bb7699a6
EQwon <bluetwintail0906@gmail.com>
2020-02-25 13:34:52 +0900

    info 바꾸고 픽요없는 코드 삭제

--- src/unity/Assets/Sercle_NetworkLibrary/Infra/Info.cs
+++ src/unity/Assets/Sercle_NetworkLibrary/Infra/Info.cs
@@ -8,10 +8,10 @@ public class Info
     // 최하단 : LOCAL 
     
     //public static string IP = "13.125.252.198";
-     public static string IP = "192.168.0.103";
+     public static string IP = "192.168.0.116";
     //public static string IP = "127.0.0.1";
 
     // public static string URL = "http://127.0.0.1:8082/";
-    public static string URL = "http://192.168.0.103:8082/";
+    public static string URL = "http://192.168.0.116:8082/";
     //public static string URL = "http://13.125.252.198:8082/";
 }
